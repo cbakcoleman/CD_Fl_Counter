8fdb85590745c74bab7e9daf5ea463e28c36a403
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-09-05 17:07:30 -0700

    complete counter add and reset

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = 'commit often'
 
-@app.route('/counter')
 def counter():
     if 'visits' in session:
         session['visits'] = session.get('visits') + 1
@@ -10,24 +9,20 @@ def counter():
         session['visits'] = 1
     return "{}".format(session.get('visits'))
 
+@app.route('/add')
+def add():
+    counter()
+    return redirect('/')
 
 @app.route('/')
 def view_counter():
+    counter()
     return render_template("index.html", session=session)
 
-
-
-
-#def contact():
-    if request.method == 'POST':
-        if request.form['add'] == session['counter'] +2:
-            pass
-        elif request.form['reset'] == session['counter'] - session['counter'] +1:
-            pass
-
-        elif request.method == 'GET':
-            return render_template('contact.html', form=form)
-
+@app.route('/clear')
+def clear_counter():
+    session.clear()
+    return redirect('/')
 
 if __name__=="__main__":
     app.run(debug=True)
